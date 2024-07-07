f0fdccbe94b937774b2c9b62a28d0ab0b0ed6843
thorreactdev <prajapativinay140404@gmail.com>
2024-07-07 20:39:22 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch(err =>{
+    console.log(err);
+});
 
 const app = express();
 
